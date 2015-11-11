f9b73a83db328002b228450dd56c6aa045c51fd6
Antoshka <Anton.tsarikovich@gmail.com>
2015-11-12 01:47:24 +0300

    1/2 Interface + save password

---
+++ code/Helper/app/src/main/java/Controller.java
@@ -0,0 +1,9 @@
+import com.example.antontsarikovich.helper.FirstWindowActivity;
+
+/**
+ * Created by Anton Tsarikovich on 11.11.2015.
+ */
+public class Controller {
+    public FirstWindowActivity firstWindowActivity;
+    public NetworkLoader networkLoader;
+}
