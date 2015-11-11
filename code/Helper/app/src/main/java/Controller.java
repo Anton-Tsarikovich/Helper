bf73a0055bf7964812adae58a076fe7cc95913ce
Antoshka <Anton.tsarikovich@gmail.com>
2015-11-12 01:53:01 +0300

    1

--- code/Helper/app/src/main/java/Controller.java
+++
@@ -1,9 +0,0 @@
-import com.example.antontsarikovich.helper.FirstWindowActivity;
-
-/**
- * Created by Anton Tsarikovich on 11.11.2015.
- */
-public class Controller {
-    public FirstWindowActivity firstWindowActivity;
-    public NetworkLoader networkLoader;
-}
